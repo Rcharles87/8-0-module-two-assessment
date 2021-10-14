027c445c6d25023115b1a875f4cdf3a56c990939
rae charles <r_charles87@yahoo.com>
2021-10-14 12:29:41 -0400

    display adds relevant text to page

--- scripts.js
+++ scripts.js
@@ -2,12 +2,37 @@ fetch("https://ghibliapi.herokuapp.com/films")
 .then((res)=>{
     return res.json();
 }).then((data)=>{
+
    
     let movieList = document.querySelector("#movie-selection")
     for (let movie of data){
+        // console.log(movie)
         let option = document.createElement("option");
         option.textContent = movie.title
         movieList.append(option)
-    }
 
+    }
+    
+    movieList.addEventListener("change", (event)=>{
+        event.preventDefault()
+        
+        for (let info of data){
+            if (info.title === event.target.value){
+        let displayInfo = document.querySelector("#display-info")
+        let h3 = document.createElement("h3");
+        let releaseYear = document.createElement("p");
+        let movieDescription = document.createElement("p");
+
+            h3.textContent = info.title;
+            releaseYear.textContent = info.release_date;
+            movieDescription.textContent =info.description;
+
+            displayInfo.append(h3, releaseYear, movieDescription);
+            }
+        }
+    })
+
+
+    
+    
 })
