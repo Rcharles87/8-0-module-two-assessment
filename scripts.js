dd3dd2705826dc362bc90fdee35e5ad571d4dafd
rae charles <r_charles87@yahoo.com>
2021-10-14 14:55:23 -0400

    finished lab and finsihed styling

--- scripts.js
+++ scripts.js
@@ -12,27 +12,44 @@ fetch("https://ghibliapi.herokuapp.com/films")
         movieList.append(option)
 
     }
+    let h3 = document.createElement("h3");
+    let releaseYear = document.createElement("p");
+    let movieDescription = document.createElement("p");
+    let displayInfo = document.querySelector("#display-info")
     
     movieList.addEventListener("change", (event)=>{
         event.preventDefault()
         
         for (let info of data){
             if (info.title === event.target.value){
-        let displayInfo = document.querySelector("#display-info")
-        let h3 = document.createElement("h3");
-        let releaseYear = document.createElement("p");
-        let movieDescription = document.createElement("p");
-
-            h3.textContent = info.title;
-            releaseYear.textContent = info.release_date;
-            movieDescription.textContent =info.description;
-
-            displayInfo.append(h3, releaseYear, movieDescription);
+                
+                h3.textContent = info.title;
+                releaseYear.textContent = info.release_date;
+                movieDescription.textContent =info.description;
+                
             }
         }
+        
+        displayInfo.append(h3, releaseYear, movieDescription);
+    })
+    
+    
+    let form = document.querySelector("#user-review")
+    form.addEventListener("submit", (event)=>{
+        event.preventDefault()
+        let userInput = event.target["user-input"].value
+        let ul = document.querySelector("ul")
+        let li = document.createElement("li")
+        let strong = document.createElement("strong")
+
+        strong.textContent = `${movieList.value}: `
+        console.log(strong.textContent)
+        li.textContent =  userInput
+        li.prepend(strong)
+        ul.append(li)
+        
+        event.target.reset()
     })
 
-
-    
     
 })
